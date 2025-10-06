bdde8a74ff895e3ca0a0c2d561d364e6b6555310
Deepak Sharma <dsharma2828@gmail.com>
2025-10-06 23:17:37 +0530

    Day 4 : Testing changes

--- src/main/java/com/ecom/Models/User.java
+++ src/main/java/com/ecom/Models/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -13,6 +14,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
